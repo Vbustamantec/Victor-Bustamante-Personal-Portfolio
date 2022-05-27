1e43411f65382b6db23b7f76c60fcdb5bf85d29b
Victor Bustamante Cera <victorbust.c@gmail.com>
2022-05-27 13:03:28 -0400

    Reducing file size and font contrast
    Reducing file size and font contrast

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,7 +1,4 @@
 import email from "../assets/email.png";
-import mobile from "../assets/mobile.png";
-import api from "../assets/api.png";
-import cpp from "../assets/cpp.png";
 import css from "../assets/css.png";
 import figma from "../assets/figma.png";
 import flutter from "../assets/flutter.png";
@@ -16,36 +13,19 @@ import react from "../assets/react.png";
 import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
-import nextjs from "../assets/nextjs.webp";
 import vue from "../assets/vue.png";
 
 import about01 from "../assets/about01.png";
-import about02 from "../assets/about02.png";
-import about03 from "../assets/about03.png";
-import about04 from "../assets/about04.png";
 
-import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 import logoDarkMode from "../assets/logoDarkMode.png";
-import backgroundHeader from "../assets/backgroundHeader.png";
-
-import adidas from "../assets/adidas.png";
-import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
-import spotify from "../assets/spotify.png";
+import backgroundHeader from "../assets/backgroundHeader.webp";
 
 export default {
   logoDarkMode,
-  nextjs,
   backgroundHeader,
   email,
-  mobile,
-  api,
-  cpp,
   css,
   figma,
   flutter,
@@ -62,17 +42,6 @@ export default {
   typescript,
   vue,
   about01,
-  about02,
-  about03,
-  about04,
-  profile,
   circle,
   logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
 };
